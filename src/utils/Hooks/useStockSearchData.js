61a3b8cc363f21e4ffea7fd74fe112c4372e9b73
Anchit Sinha <anchit1909@gmail.com>
2023-10-24 16:12:05 +0530

    Implemented Ticker Search API

---
+++ src/utils/Hooks/useStockSearchData.js
@@ -0,0 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+export const useStockSearchInfo = (searchKey) => {
+  const SECRET_KEY = process.env.NEXT_PUBLIC_ALPHA_API_KEY;
+  const URL = ` ${"https://www.alphavantage.co/query?function="}${"SYMBOL_SEARCH"}&keywords=${searchKey}&apikey=${SECRET_KEY}`;
+  return useQuery({
+    queryKey: ["SYMBOL_SEARCH", searchKey],
+    queryFn: async () => {
+      const { data } = await axios.get(URL);
+      if (data.Note) {
+        throw new Error(data.Note);
+      } else if (data.Information) {
+        throw new Error(data.Information);
+      }
+
+      return data.bestMatches;
+    },
+    staleTime: 5 * 60 * 1000,
+  });
+};
