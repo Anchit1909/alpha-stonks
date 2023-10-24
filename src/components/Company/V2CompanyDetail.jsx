5fd4bfbc972b5ff769665ebfbd7d6780307990e6
Anchit Sinha <anchit1909@gmail.com>
2023-10-25 03:00:10 +0530

    Company Page UI changes

--- src/components/Company/V2CompanyDetail.jsx
+++ src/components/Company/V2CompanyDetail.jsx
@@ -46,106 +46,172 @@ const V2CompanyDetail = ({ TikerValue }) => {
               <span className="text-sm font-medium">{info?.Currency}</span>
             </div>
 
-            <div className="text-sm text-muted-foreground border-t py-4">
-              <div className="flex gap-4 items-center">
-                <Badge className="text-[12px] rounded-full text-white py-1">
-                  SECTOR : {info?.Sector}
-                </Badge>
-                <Badge className="text-[12px] rounded-full text-white py-1">
-                  INDUSTRY: {info?.Industry}
-                </Badge>
-              </div>
-              <div className="flex justify-start items-center flex-wrap pt-4 gap-4">
-                <div className="flex justify-between items-center pt-2 text-accent-foreground font-medium">
-                  <span className="text-sm">About {info?.Name}</span>
+            <div className="text-sm text-muted-foreground border-t">
+              <div className="flex flex-col lg:flex-row justify-between pt-4 gap-4">
+                <div className="flex justify-start w-full lg:w-3/4 flex-col pt-2 text-accent-foreground font-medium">
+                  <div className="flex gap-4 items-center py-4">
+                    <Badge className="text-[12px] rounded-full text-white py-1">
+                      SECTOR : {info?.Sector}
+                    </Badge>
+                    <Badge className="text-[12px] rounded-full text-white py-1">
+                      INDUSTRY: {info?.Industry}
+                    </Badge>
+                  </div>
+                  <span className="text-sm text-accent-foreground ">
+                    About {info?.Name}
+                  </span>
+                  <p className="text-muted-foreground">{info?.Description}</p>
+                  <div className="flex flex-row gap-4 pt-4 flex-wrap">
+                    <Card className="flex-col flex justify-center items-center">
+                      <CardHeader>
+                        <CardTitle>50 Day Moving Average</CardTitle>
+                        <CardDescription className="text-primary">
+                          ${info?.["50DayMovingAverage"]}
+                        </CardDescription>
+                      </CardHeader>
+                    </Card>
+                    <Card className="flex-col flex justify-center items-center">
+                      <CardHeader>
+                        <CardTitle>200 Day Moving Average</CardTitle>
+                        <CardDescription className="text-primary">
+                          ${info?.["200DayMovingAverage"]}
+                        </CardDescription>
+                      </CardHeader>
+                    </Card>
+                    <Card className="flex-col flex justify-center items-center">
+                      <CardHeader>
+                        <CardTitle>50 Day Moving Average</CardTitle>
+                        <CardDescription className="text-primary">
+                          ${info?.["50DayMovingAverage"]}
+                        </CardDescription>
+                      </CardHeader>
+                    </Card>
+                    <div className="lg:hidden flex justify-start items-center flex-wrap pt-4 gap-4">
+                      <Card className="flex-col flex justify-center items-center">
+                        <CardHeader>
+                          <CardTitle>Market Cap</CardTitle>
+                          <CardDescription className="text-primary">
+                            {Number(info?.MarketCapitalization)}
+                          </CardDescription>
+                        </CardHeader>
+                      </Card>
+                      <Card className="flex-col flex justify-center items-center">
+                        <CardHeader>
+                          <CardTitle> EBITDA</CardTitle>
+                          <CardDescription className="text-primary">
+                            {Number(info?.EBITDA)}
+                          </CardDescription>
+                        </CardHeader>
+                      </Card>
+                      <Card className="flex-col flex justify-center items-center">
+                        <CardHeader>
+                          <CardTitle> PEG Ratio</CardTitle>
+                          <CardDescription className="text-primary">
+                            {info?.PEGRatio}
+                          </CardDescription>
+                        </CardHeader>
+                      </Card>
+                      <Card className="flex-col flex justify-center items-center">
+                        <CardHeader>
+                          <CardTitle> P/E Ratio</CardTitle>
+                          <CardDescription className="text-primary">
+                            {info?.PERatio}
+                          </CardDescription>
+                        </CardHeader>
+                      </Card>
+                      <Card className="flex-col flex justify-center items-center">
+                        <CardHeader>
+                          <CardTitle> Beta</CardTitle>
+                          <CardDescription className="text-primary">
+                            {info?.Beta}
+                          </CardDescription>
+                        </CardHeader>
+                      </Card>
+                      <Card className="flex-col flex justify-center items-center">
+                        <CardHeader>
+                          <CardTitle> Profit Margin</CardTitle>
+                          <CardDescription className="text-primary">
+                            {info?.ProfitMargin}
+                          </CardDescription>
+                        </CardHeader>
+                      </Card>
+                      <Card className="flex-col flex justify-center items-center">
+                        <CardHeader>
+                          <CardTitle> Dividend Yield</CardTitle>
+                          <CardDescription className="text-primary">
+                            {info?.DividendYield}%
+                          </CardDescription>
+                        </CardHeader>
+                      </Card>
+                      <Card className="flex-col flex justify-center items-center">
+                        <CardHeader>
+                          <CardTitle> EPS</CardTitle>
+                          <CardDescription className="text-primary">
+                            {info?.EPS}
+                          </CardDescription>
+                        </CardHeader>
+                      </Card>
+                      <Card className="flex-col flex justify-center items-center">
+                        <CardHeader>
+                          <CardTitle> Analyst Target Price</CardTitle>
+                          <CardDescription className="text-primary">
+                            ${info?.AnalystTargetPrice}
+                          </CardDescription>
+                        </CardHeader>
+                      </Card>
+                      <Card className="flex-col flex justify-center items-center">
+                        <CardHeader>
+                          <CardTitle>52 Week High</CardTitle>
+                          <CardDescription className="text-primary">
+                            ${info?.["52WeekHigh"]}
+                          </CardDescription>
+                        </CardHeader>
+                      </Card>
+                      <Card className="flex-col flex justify-center items-center">
+                        <CardHeader>
+                          <CardTitle>52 Week Low</CardTitle>
+                          <CardDescription className="text-primary">
+                            ${info?.["52WeekLow"]}
+                          </CardDescription>
+                        </CardHeader>
+                      </Card>
+                    </div>
+                  </div>
                 </div>
-                {info?.Description}
-                <div className="flex justify-start items-center flex-wrap pt-4 gap-4">
-                  <Card className="flex-col flex justify-center items-center">
+                <div className="hidden lg:flex justify-start w-full lg:w-1/4 items-center flex-wrap pt-4 gap-4">
+                  <Card className="flex-col flex justify-start items-start flex-1">
                     <CardHeader>
                       <CardTitle>Market Cap</CardTitle>
-                      <CardDescription className="text-primary">
+                      <CardDescription className="text-primary border-b">
                         {Number(info?.MarketCapitalization)}
                       </CardDescription>
-                    </CardHeader>
-                  </Card>
-                  <Card className="flex-col flex justify-center items-center">
-                    <CardHeader>
                       <CardTitle> EBITDA</CardTitle>
-                      <CardDescription className="text-primary">
+                      <CardDescription className="text-primary border-b">
                         {Number(info?.EBITDA)}
                       </CardDescription>
-                    </CardHeader>
-                  </Card>
-                  <Card className="flex-col flex justify-center items-center">
-                    <CardHeader>
-                      <CardTitle> PEG Ratio</CardTitle>
-                      <CardDescription className="text-primary">
-                        {info?.PEGRatio}
+                      <CardTitle> P/E Ratio</CardTitle>
+                      <CardDescription className="text-primary border-b">
+                        {info?.PERatio}
                       </CardDescription>
-                    </CardHeader>
-                  </Card>
-                  <Card className="flex-col flex justify-center items-center">
-                    <CardHeader>
+                      <CardTitle> Beta</CardTitle>
+                      <CardDescription className="text-primary border-b">
+                        {info?.Beta}
+                      </CardDescription>
+                      <CardTitle> Profit Margin</CardTitle>
+                      <CardDescription className="text-primary border-b">
+                        {info?.ProfitMargin}
+                      </CardDescription>
                       <CardTitle> Dividend Yield</CardTitle>
-                      <CardDescription className="text-primary">
-                        {info?.DividendYield}
+                      <CardDescription className="text-primary border-b">
+                        {info?.DividendYield}%
                       </CardDescription>
-                    </CardHeader>
-                  </Card>
-                  <Card className="flex-col flex justify-center items-center">
-                    <CardHeader>
-                      <CardTitle> EPS</CardTitle>
-                      <CardDescription className="text-primary">
-                        {info?.EPS}
-                      </CardDescription>
-                    </CardHeader>
-                  </Card>
-                  <Card className="flex-col flex justify-center items-center">
-                    <CardHeader>
-                      <CardTitle> Analyst Target Price</CardTitle>
-                      <CardDescription className="text-primary">
-                        ${info?.AnalystTargetPrice}
-                      </CardDescription>
-                    </CardHeader>
-                  </Card>
-                  <Card className="flex-col flex justify-center items-center">
-                    <CardHeader>
                       <CardTitle>52 Week High</CardTitle>
-                      <CardDescription className="text-primary">
+                      <CardDescription className="text-primary border-b">
                         ${info?.["52WeekHigh"]}
                       </CardDescription>
-                    </CardHeader>
-                  </Card>
-                  <Card className="flex-col flex justify-center items-center">
-                    <CardHeader>
                       <CardTitle>52 Week Low</CardTitle>
                       <CardDescription className="text-primary">
                         ${info?.["52WeekLow"]}
-                      </CardDescription>
-                    </CardHeader>
-                  </Card>
-                  <Card className="flex-col flex justify-center items-center">
-                    <CardHeader>
-                      <CardTitle>50 Day Moving Average</CardTitle>
-                      <CardDescription className="text-primary">
-                        ${info?.["50DayMovingAverage"]}
-                      </CardDescription>
-                    </CardHeader>
-                  </Card>
-                  <Card className="flex-col flex justify-center items-center">
-                    <CardHeader>
-                      <CardTitle>200 Day Moving Average</CardTitle>
-                      <CardDescription className="text-primary">
-                        ${info?.["200DayMovingAverage"]}
-                      </CardDescription>
-                    </CardHeader>
-                  </Card>
-                  <Card className="flex-col flex justify-center items-center">
-                    <CardHeader>
-                      <CardTitle>50 Day Moving Average</CardTitle>
-                      <CardDescription className="text-primary">
-                        ${info?.["50DayMovingAverage"]}
                       </CardDescription>
                     </CardHeader>
                   </Card>
